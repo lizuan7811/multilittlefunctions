cc7de6a5d58f220f21bb41f9de78f89b57f8a761
7164 李囿人 marsli <marsli@pershing.com.tw>
2023-03-07 15:45:53 +0800

    finish simple log call

--- src/main/java/monitorsysinfoutil/SystemInfoMonitor.java
+++ src/main/java/monitorsysinfoutil/SystemInfoMonitor.java
@@ -14,41 +14,53 @@ import oshi.hardware.GlobalMemory;
 @Component
 public class SystemInfoMonitor {
 
+	private static long[] prevTicks = null;
+	private static long[] ticks = null;
+
 	private SystemInfo getSystemInfo() {
 		return new SystemInfo();
 	}
 
 	public String readSystemCpuInfo() {
-		
-		System.out.println("readSystemCpuInfo");
 
 		CentralProcessor centralprocess = getSystemInfo().getHardware().getProcessor();
-		long[] prevTicks = centralprocess.getSystemCpuLoadTicks();
-		long[] ticks = centralprocess.getSystemCpuLoadTicks();
+		if (prevTicks == null) {
+			prevTicks = centralprocess.getSystemCpuLoadTicks();
+		}
+		ticks = centralprocess.getSystemCpuLoadTicks();
 
 		long nice = ticks[CentralProcessor.TickType.NICE.getIndex()]
 				- prevTicks[CentralProcessor.TickType.NICE.getIndex()];
+		log.debug("CPU nice: {}", nice);
 		long irq = ticks[CentralProcessor.TickType.IRQ.getIndex()]
 				- prevTicks[CentralProcessor.TickType.IRQ.getIndex()];
+		log.debug("CPU nice: {}", irq);
 		long softirq = ticks[CentralProcessor.TickType.SOFTIRQ.getIndex()]
 				- prevTicks[CentralProcessor.TickType.SOFTIRQ.getIndex()];
+		log.debug("CPU softirq: {}", softirq);
 		long steal = ticks[CentralProcessor.TickType.STEAL.getIndex()]
 				- prevTicks[CentralProcessor.TickType.STEAL.getIndex()];
+		log.debug("CPU steal: {}", steal);
 		long cSys = ticks[CentralProcessor.TickType.SYSTEM.getIndex()]
 				- prevTicks[CentralProcessor.TickType.SYSTEM.getIndex()];
+		log.debug("CPU cSys: {}", cSys);
 		long user = ticks[CentralProcessor.TickType.USER.getIndex()]
 				- prevTicks[CentralProcessor.TickType.USER.getIndex()];
+		log.debug("CPU user: {}", user);
 		long iowait = ticks[CentralProcessor.TickType.IOWAIT.getIndex()]
 				- prevTicks[CentralProcessor.TickType.IOWAIT.getIndex()];
+		log.debug("CPU iowait: {}", iowait);
 		long idle = ticks[CentralProcessor.TickType.IDLE.getIndex()]
 				- prevTicks[CentralProcessor.TickType.IDLE.getIndex()];
+		log.debug("CPU idle: {}", idle);
 
 		long totalCpu = idle + iowait + user + cSys + steal + softirq + irq + nice;
 		System.out.println(totalCpu);
-
+		log.debug("CPU totalCpu: {}", totalCpu);
 		String result = String.format("CPU 總量: %s , CPU使用率: %s", centralprocess.getLogicalProcessorCount(),
 				new DecimalFormat("#.##%").format(1.0 - (idle * 1.0 / totalCpu)));
 		log.debug("CPU Result: {}", result);
+		this.prevTicks = ticks;
 
 		sleepOneSecond();
 		return result;
--- src/main/java/multilittlefunctioins/MultiLittleFunctionsStart.java
+++ src/main/java/multilittlefunctioins/MultiLittleFunctionsStart.java
@@ -4,14 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.extern.slf4j.Slf4j;
-@Slf4j
 @SpringBootApplication(scanBasePackages = {"multilittlefunctioins", "monitorsysinfoutil"})
 public class MultiLittleFunctionsStart {
 
 	public static void main(String[] args) {
-		log.debug("Start MultiLittleFunctionsStart");
 		SpringApplication.run(MultiLittleFunctionsStart.class,args);
 	}
-
 }
 
